a3ab5e8175d201fac557d69c220cee91eedfad44
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-02-18 17:31:19 +0330

    change js to jsx

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+import * as Comps from "@/components";
+
+const DATA = [
+  { image: "https://picsum.photos/seed/random101/500/500" },
+  { image: "https://picsum.photos/seed/random102/500/500" },
+  { image: "https://picsum.photos/seed/random103/500/500" },
+];
+
+export default function Home() {
+  return (
+    <main>
+      <Comps.Slider data={DATA} />
+    </main>
+  );
+}
