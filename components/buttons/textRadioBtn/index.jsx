5eacc6164567c27a504232d2c4e768aef80c9189
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-02-19 13:06:45 +0330

    change radioBtn to TextRadioBtn

---
+++ components/buttons/textRadioBtn/index.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import styles from "./index.module.scss";
+
+const TextRadioBtn = (props) => {
+  const { id, name, value, checked, onClick } = props;
+
+  return (
+    <div className={styles["radio-container"]}>
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        checked={checked || false}
+        onClick={onClick}
+      />
+      <label for={id}>{name}</label>
+    </div>
+  );
+};
+
+export default TextRadioBtn;
--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
 export { default as Slider } from "./slider";
 export { default as Details } from "./details";
-export { default as RadioBtn } from "./radioBtn";
+export { default as TextRadioBtn } from "./buttons/textRadioBtn";
